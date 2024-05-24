7f3712c2ab45d8b2db7f46d13ec1d15b76c49e7d
danbzg <danbzg@gmail.com>
2024-05-24 15:03:35 +0300

    final v

--- AiogramBot.py
+++ AiogramBot.py
@@ -2,6 +2,8 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 import asyncio
 from aiogram.types.web_app_info import WebAppInfo
+from aiogram import F
+import json
 
 
 bot = Bot('7166099528:AAEZ--6lRWUIezdRAVelCT5n6U7AyZEwU1w')
@@ -13,8 +15,10 @@ async def start(message: types.Message):
     markup = types.ReplyKeyboardMarkup(keyboard=[[b1]])
     await message.answer('Привет, мой друг!', reply_markup=markup)
 
-
-
+@dp.message(F.content_type.in_({'web_app_data'}))
+async def web_app(message: types.Message):
+    res = json.loads(message.web_app_data.data)
+    await message.answer(f'Name: {res["name"]}. Email: {res["email"]}. Phone: {res["phone"]}')
 
 
 
